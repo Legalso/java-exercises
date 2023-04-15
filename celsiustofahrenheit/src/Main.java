b51ae2feaf76df1c02f3a46ac2a622b39162eeb1
caiopatriota <caiopatriotabarros@gmail.com>
2023-04-15 05:07:02 -0300

    First exercise v2

--- celsiustofahrenheit/src/Main.java
+++ celsiustofahrenheit/src/Main.java
@@ -2,13 +2,26 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Conversor de temperaturas: Celsius --> Fahrenheit");
 
-        int celsius = 25;
-        int temperatura;
-        temperatura = celsius;
+        double celsius = 25.5;
         System.out.println("A temperatura em Celsius é: " + celsius +"°C");
 
         // Dica: A fórmula para converter temperaturas de graus Celsius para Fahrenheit é: (temperatura * 1.8) + 32.
-        int fahrenheit = (int) ((temperatura * 1.8) + 32);
+        int fahrenheit = (int) ((celsius * 1.8) + 32);
         System.out.println("A temperatura convertida para Fahrenheit é: " + fahrenheit +"°F");
     }
+
+    /* Resposta correta:
+    public static void main(String[] args) {
+        double temperaturaEmCelsius = 30.4;
+        double temperaturaEmFahrenheit = (temperaturaEmCelsius * 1.8) + 32;
+
+        String mensagem = String.format("A temperatura de %f Celsius é equivalente a %f Fahrenheit", temperaturaEmCelsius, temperaturaEmFahrenheit);
+
+        System.out.println(mensagem);
+
+        int temperaturaEmFahrenheitInteira = (int) temperaturaEmFahrenheit;
+        System.out.println("A temperatura em Fahrenheit inteira é: " + temperaturaEmFahrenheitInteira);
+
+    }
+    */
 }
