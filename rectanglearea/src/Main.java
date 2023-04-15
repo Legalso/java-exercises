39da2fd5b6d45d846dd67db7b80d3c8240541f28
caiopatriota <caiopatriotabarros@gmail.com>
2023-04-15 04:55:20 -0300

    Second exercise

---
+++ rectanglearea/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Calculadora de área do Retângulo");
+
+        int base = 5;
+        int altura = 10;
+        System.out.println("A base do retângulo é: " + base);
+        System.out.println("A altura do retângulo é: " + altura);
+
+        // Dica: A fórmula para calcular a área do retângulo é A=b.h (base x altura)
+
+        int area = base * altura;
+        System.out.println("A área do retângulo é: " + area);
+    }
+}
